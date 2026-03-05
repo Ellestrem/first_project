621b57f5629197eca1e4555f03967856c94e7639
ellestrem <ellestrem@gmail.com>
2026-03-05 21:13:11 +0500

    add .env

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Hello from repository!')
+# print('Hello from repository!')
+
+# Импорт load_dotenv.
+from dotenv import load_dotenv 
+
+# Импорт библиотеки для работы с окружением.
+import os  
+
+def print_author():
+    # Загрузка переменных из .env
+    load_dotenv(dotenv_path='/Users/ell/documents/study/terminal/my_project/first_project/.env')
+
+    # Теперь переменные доступны через os.environ
+    author = os.getenv('AUTHOR')
+
+    print(f"Автор проекта: {author}")
+
+print_author()
